2b1d802c9186a09ea35240e323a084fbb48e7735
Ashvin <ashvinkausar@gmail.com>
2024-07-11 23:31:56 +0530

    new

---
+++ basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version
+// alert("hello"); we are using nodejs not browser
+
+//code readability should be high
+
+let name="Ashvin"
+let age=18
+let isLoggedIn=false
+let notDefined;
+
+// primitive data types
+//bigint => when the number exceeds the range of number
+//string => '' , "" prefer ""
+//boolean => true.false
+//null => standalone value
+//undefined => abhi value assign nahi hui h
+//symbol => unique
+
+// objects
+//if you write null with typeof then it will give object
+console.log(typeof undefined);
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,17 @@
-console.log("Ashvin"); 
+const accountId = 144553
+let accountEmail="devanshashwin@gmail.com"
+var accountPassword="12345";
+accountCity="Jaipur";
+let accountState;
+
+// accountId=6788934 not allowed here
+
+accountEmail="devansh@gmail.com"
+
+/*
+Prefer not to use var
+Because of use in block scope
+ */
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountCity,accountPassword,accountState]);
